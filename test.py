58581ea952629829e2be0a5584ddd5c126f1b45e
jiawu3 <jia.wu3@gmail.com>
2018-12-30 00:53:11 -0500

    Adding python test file

---
+++ test.py
@@ -0,0 +1,19 @@
+
+# %%
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 2np.pi, 1000)
+y = np.sin(x)
+plt.plot(x, np.sin(x))
+plt.show()
+
+
+def update(hello)
+    return hello
+
+
+# %%
+programming_languages = Python, C++, C#, Java
+for langauges in programming_languages
+    print(langauges)
